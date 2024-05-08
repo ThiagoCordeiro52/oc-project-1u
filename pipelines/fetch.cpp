ab8e7641fb3b1290cea48336f595002e66e85f51
André Freitas <freitasandre38@gmail.com>
2024-05-08 10:25:07 -0300

    feat: create fetch pipeline

---
+++ pipelines/fetch.cpp
@@ -0,0 +1,20 @@
+#include "systemc.h"
+#include "sysc/utils/sc_vector.h"
+
+SC_MODULE(fetch)
+{
+    sc_in_clk clock;
+    sc_in<sc_uint<32>> input;
+    sc_out<sc_uint<32>> output;
+    void next();
+
+    SC_CTOR(fetch)
+    {
+        SC_METHOD(next);
+        sensitive << clock.pos();
+    }
+};
+
+void fetch::next() {
+    output.write(input.read());
+}
