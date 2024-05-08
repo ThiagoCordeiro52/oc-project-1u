75b3216bf1951729910e5e733b60ab26d7d8b41a
André Freitas <freitasandre38@gmail.com>
2024-05-08 09:55:08 -0300

    refactor: change main structure

--- main.cpp
+++ main.cpp
@@ -1,18 +1,43 @@
-#include "systemc.h"
+#include <systemc.h>
 
-#include <vector>
-#include <string>
-#include <sstream>
-#include <fstream>
+// Include all your processor components
+#include "ProgramCounter.h"
+#include "ALU.h"
+#include "Register.h"
+#include "RegisterDatabase.h"
+#include "InstructionMemory.h"
+#include "Memory.h"
+#include "MUX.h"
 
-#include "common/control/control.hpp"
-#include "common/data-memory/data-memory.cpp"
-#include "common/instruction-memory/instruction-memory.cpp"
-#include "common/mux/mux.cpp"
-#include "common/pc/pc.cpp"
-#include "common/registers/registers.cpp"
-#include "common/ula/ula.cpp"
+int sc_main(int argc, char* argv[]) {
+    ProgramCounter pc("PC");
+    ALU alu("ALU");
+    Register reg1("Reg1");
+    Register reg2("Reg2");
+    RegisterDatabase regDB("RegDB");
+    InstructionMemory imem("IMem");
+    Memory mem("Mem");
+    MUX mux("MUX");
 
-int sc_main(int argc, char *argv[]){
+    pc.clock(clock);
+    pc.reset(reset);
+    pc.enable(enable);
+    pc.jump(jump);
+    pc.jumpAddress(jumpAddress);
+    pc.address(pc_address);
 
+    alu.operand1(operand1);
+    alu.operand2(operand2);
+    alu.alu_control(alu_control);
+    alu.result(alu_result);
+    alu.zero(alu_zero);
+
+    reg1.clock(clock);
+    reg1.reset(reset);
+    reg1.enable(enable);
+    reg1.data_in(reg1_data_in);
+    reg1.data_out(reg1_data_out);
+
+
+    return 0;
 }
