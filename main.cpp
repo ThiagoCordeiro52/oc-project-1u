bb09a401b85251a58168ba11ccb914f43a334514
André Freitas <freitasandre38@gmail.com>
2024-05-08 09:28:26 -0300

    refactor: change project structure

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "systemc.h"
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+#include "common/control/control.hpp"
+#include "common/data-memory/data-memory.cpp"
+#include "common/instruction-memory/instruction-memory.cpp"
+#include "common/mux/mux.cpp"
+#include "common/pc/pc.cpp"
+#include "common/registers/registers.cpp"
+#include "common/ula/ula.cpp"
+
+int sc_main(int argc, char *argv[]){
+
+}
