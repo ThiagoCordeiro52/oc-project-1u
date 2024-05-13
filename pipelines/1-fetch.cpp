6e800790aed2c6fbcf0a64a1308152eb2925ac99
André Freitas <freitasandre38@gmail.com>
2024-05-13 20:03:20 -0300

    refactor: fix problem with 2 decode

--- pipelines/1-fetch.cpp
+++ pipelines/1-fetch.cpp
@@ -1,15 +1,13 @@
 #include "systemc.h"
 #include "sysc/utils/sc_vector.h"
 
-SC_MODULE(fetch)
-{
+SC_MODULE(fetch) { // Ok
     sc_in_clk clock;
     sc_in<sc_uint<32>> input;
     sc_out<sc_uint<32>> output;
     void next();
 
-    SC_CTOR(fetch)
-    {
+    SC_CTOR(fetch) {
         SC_METHOD(next);
         sensitive << clock.pos();
     }
---
+++ pipelines/2-decode.cpp
@@ -0,0 +1,40 @@
+#include "systemc.h"
+#include "sysc/utils/sc_vector.h"
+
+SC_MODULE(decode) { // Ok
+    sc_in_clk clock;
+    sc_in<sc_uint<32>> instructionInput, dataInput1, dataInput2;
+    sc_in<sc_uint<32>> memoryAddressLoadInput, ulaAddressInput, memoryLoadAddressIn, memoryWriteAddressIn;
+    sc_in<bool> ulaOperatorIn, controlMemoryInput, jumpInput, writeInput, loadMemoryInput, jumpCompareInput, jumpNegInput;
+
+    sc_out<sc_uint<32>> instructionOutput, dataOutput1, dataOutput2;
+    sc_out<sc_uint<5>> memoryLoadAddressOutput, ulaAddressOutput, memoryLoadAddressOutput, memoryWriteAddressOutput;
+    sc_out<bool> ulaOperatorOutput, controlMemoryWriteOutput, jumpOutput, regWriteOutput, memoryLoadOutput, jumpCompareOutput, jumpNegOutput;
+
+    void next();
+
+    SC_CTOR(decode)
+    {
+        SC_METHOD(next);
+        sensitive << clock.pos();
+    }
+};
+
+void decode::next() {
+
+    instructionOutput.write(instructionInput.read());
+    dataOutput1.write(dataInput1.read());
+    dataOutput2.write(dataInput2.read());
+    memoryLoadAddressOutput.write(memoryAddressLoadInput.read().range(25, 21));
+    ulaAddressOutput.write(ulaAddressInput.read().range(15, 11));
+    memoryLoadAddressOutput.write(memoryLoadAddressIn.read().range(20, 16));
+    AddrMemWriteOut.write(memoryWriteAddressIn.read().range(20, 16));
+
+    ulaOperatorOutput.write(ulaOperatorIn.read());
+    controlMemoryWriteOutput.write(controlMemoryInput.read());
+    jumpOutput.write(jumpInput.read());
+    regWriteOutput.write(writeInput.read());
+    memoryLoadOutput.write(loadMemoryInput.read());
+    jumpCompareOutput.write(jumpCompareInput.read());
+    jumpNegOutput.write(jumpNegInput.read());
+}
