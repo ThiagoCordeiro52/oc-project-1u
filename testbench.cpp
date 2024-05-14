2201ab92e729123c6081b0e9dbdcb29681ae0972
André Freitas <freitasandre38@gmail.com>
2024-05-14 19:52:54 -0300

    feat: finish testbench

--- testbench.cpp
+++ testbench.cpp
@@ -40,4 +40,94 @@ SC_MODULE(Testbench) {
     sc_signal<sc_uint<32>> mux_operand1, mux_operand2, mux_operand3, mux_result;
     sc_signal<bool> mux_zero;
 
+    SC_CTOR(Testbench) : pc("PC"), alu("ALU"), reg1("Reg1"), reg2("Reg2"),
+                         regDB("RegDB"), imem("IMem"), mem("Mem"),
+                         mux("MUX"), fetch("fetch"), decode("decode"),
+                         execute("execute"), access("access"), clock("clock", 10, SC_NS) {
+
+        // Connect components to signals
+        pc.clock(clock);
+        pc.reset(reset);
+        pc.enable(enable);
+        pc.jump(jump);
+        pc.jumpAddress(jumpAddress);
+        pc.address(pc_address);
+
+        alu.operand1(operand1);
+        alu.operand2(operand2);
+        alu.alu_control(alu_control);
+        alu.result(alu_result);
+        alu.zero(alu_zero);
+
+        reg1.clock(clock);
+        reg1.reset(reset);
+        reg1.enable(enable);
+        reg1.data_in(reg1_data_in);
+        reg1.data_out(reg1_data_out);
+
+        regDB.reg_id(regDB_reg_id);
+        regDB.data_in(regDB_data_in);
+        regDB.data_out(regDB_data_out);
+
+        imem.clock(clock);
+        imem.address_in(imem_address_in);
+        imem.instruction_out(imem_instruction_out);
+
+        mem.clock(clock);
+        mem.write_enable(mem_write_enable);
+        mem.read_enable(mem_read_enable);
+        mem.address(mem_address);
+        mem.data(mem_data);
+
+        mux.operand1(mux_operand1);
+        mux.operand2(mux_operand2);
+        mux.operand3(mux_operand3);
+        mux.result(mux_result);
+        mux.zero(mux_zero);
+
+        // Connect pipeline stages
+        fetch.clock(clock);
+        fetch.pc_address(pc_address);
+        fetch.imem_instruction_out(imem_instruction_out);
+
+        decode.clock(clock);
+        decode.imem_instruction_in(imem_instruction_out);
+        decode.regDB_data_out(regDB_data_out);
+
+        execute.clock(clock);
+        execute.operand1(operand1);
+        execute.operand2(operand2);
+        execute.alu_control(alu_control);
+        execute.alu_result(alu_result);
+        execute.alu_zero(alu_zero);
+
+        access.clock(clock);
+        access.mem_data(mem_data);
+        access.mem_clock(mem_clock);
+        access.mem_write_enable(mem_write_enable);
+        access.mem_read_enable(mem_read_enable);
+        access.mem_address(mem_address);
+
+        SC_THREAD(test);
+    }
+
+    void test() {
+
+        reset = 1;
+        enable = 1;
+        jump = 0;
+        jumpAddress = 0;
+        wait(10, SC_NS);
+
+        reset = 0;
+        pc_address = 0x100;
+        wait(10, SC_NS);
+
+    }
+};
+
+int sc_main(int argc, char* argv[]) {
+    Testbench tb("Testbench");
+    sc_start(); 
+    return 0;
 }
